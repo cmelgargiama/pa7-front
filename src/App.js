5314d96f7e44b0b4d4f2d5c07587daf860f0d183
arcefranco <arcefranco29@gmail.com>
2022-08-05 20:04:09 -0300

    test change 2

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
           <Route path='/' element={<Home/>}/>
           <Route path='/login' element={<Login/>}/>
         
-            
+            <Route path='/gerentes' element={<GerentesTable/>}/>
           
           
         </Routes> 
