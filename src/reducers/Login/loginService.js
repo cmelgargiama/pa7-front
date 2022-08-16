f2ac69033c916afb4508a5c6e80f1b7590c4d21e
franco <2>
2022-08-16 10:09:30 -0300

    cambios de estilo y test en home

--- src/reducers/Login/loginService.js
+++ src/reducers/Login/loginService.js
@@ -5,7 +5,7 @@ const API_URL = 'http://localhost:3001/'
 const login = async (userData) => {
 
     const response = await axios.post(API_URL + 'login', userData) 
-    window.localStorage.setItem('user', JSON.stringify(response.data))
+    window.localStorage.setItem('user', JSON.stringify(`Bearer ${response.data.token}`))
     return response.data 
   }
 
